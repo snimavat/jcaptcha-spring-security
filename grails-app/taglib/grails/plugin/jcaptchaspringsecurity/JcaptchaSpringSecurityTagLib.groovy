6fc93c4fa9b9001668453307650401f1878e32e1
Sudhir <sudhir@nimavat.me>
2015-08-15 12:32:37 +0530

    Make it possible to override _jcaptchaLogin.gsp template

--- grails-app/taglib/grails/plugin/jcaptchaspringsecurity/JcaptchaSpringSecurityTagLib.groovy
+++ grails-app/taglib/grails/plugin/jcaptchaspringsecurity/JcaptchaSpringSecurityTagLib.groovy
@@ -1,6 +1,7 @@
 package grails.plugin.jcaptchaspringsecurity
 
 import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.codehaus.groovy.grails.web.pages.discovery.GrailsConventionGroovyPageLocator
 
 /**
  * Created by sudhir on 8/14/2015.
@@ -8,12 +9,30 @@ import org.codehaus.groovy.grails.commons.GrailsApplication
 class JcaptchaSpringSecurityTagLib {
 
     GrailsApplication grailsApplication
+    GrailsConventionGroovyPageLocator groovyPageLocator
 
     def jcaptchaLogin = { attrs, body ->
         boolean enabled = (grailsApplication.config.grails.plugin.jcaptchaspringsecurity.enabled == true)
+
         if(session.jcaptchaForLogin == true && enabled){
-            out << render(template: "/templates/jcaptchaLogin", plugin: 'jcaptcha-spring-security')
+            out << render(template: template.path, plugin: template.plugin)
         }
+
+    }
+
+    /**
+     * See if template is overriden in host project, then use it.
+     * @return
+     */
+    private Map getTemplate() {
+        String path = "/templates/jcaptchaLogin"
+        Map template = [path:path]
+        def override = groovyPageLocator.findTemplateInBinding(path, pageScope)
+        if(!override) {
+            template.plugin = "jcaptcha-spring-security"
+        }
+
+        return  template
     }
 
 }
